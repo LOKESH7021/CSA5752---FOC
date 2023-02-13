d23ea4cd394272a6c4a5b1af4efa739dd51a92fc
LOKESH KUMAR <112959560+LOKESH7021@users.noreply.github.com>
2023-02-13 21:38:34 +0530

    Shell Programming

---
+++ Calculator.sh
@@ -0,0 +1,13 @@
+echo "enter a value"
+read a;
+echo "enter b value"
+read b;
+echo "addition of $a and $b:"
+echo $(( $a + $b ))
+echo "substraction of $a and $b:"
+echo $(( $a - $b ))
+echo "multiplication of $a and $b:"
+echo $(( $a * $b ))
+echo "division of $a and $b:"
+echo $(($a / $b ))
+
